22f1f56252faa13a913510bce7d659b0ac4e9893
Andres Zurita <andres.zurita8@gmail.com>
2024-03-11 18:52:39 +0100

    Define separate function to create "kokotapper"

--- internal/controller/kokotap_controller.go
+++ internal/controller/kokotap_controller.go
@@ -20,11 +20,13 @@ import (
 	"context"
 	"fmt"
 
+	errors "github.com/pkg/errors"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"sigs.k8s.io/cluster-api/util/patch"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -72,7 +74,7 @@ type PodInfo struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
-func (r *KokotapReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+func (r *KokotapReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, errResult error) {
 	logger := log.FromContext(ctx)
 
 	kokotapper := &networkingv1alpha1.Kokotap{}
@@ -86,6 +88,16 @@ func (r *KokotapReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		return ctrl.Result{}, nil
 	}
 
+	helper, err := patch.NewHelper(kokotapper, r.Client)
+	if err != nil {
+		return ctrl.Result{}, errors.Wrap(err, "failed to create patch helper")
+	}
+	defer func() {
+		// patch the resource stored in the API server if something changed.
+		if err := helper.Patch(ctx, kokotapper); err != nil {
+			errResult = err
+		}
+	}()
 	// Check if the resource is being deleted
 
 	if !kokotapper.ObjectMeta.DeletionTimestamp.IsZero() {
@@ -103,101 +115,130 @@ func (r *KokotapReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	return ctrl.Result{}, nil
 }
 
-func (r *KokotapReconciler) ReconcileNormal(ctx context.Context, req ctrl.Request, vtap *networkingv1alpha1.Kokotap) (ctrl.Result, error) {
+func (r *KokotapReconciler) GetKokotapper(ctx context.Context, req ctrl.Request) (*networkingv1alpha1.Kokotap, error) {
+	kokotapper := &networkingv1alpha1.Kokotap{}
+	if err := r.Get(ctx, req.NamespacedName, kokotapper); err != nil {
+		return nil, err
+	}
+	return kokotapper, nil
+}
 
+func (r *KokotapReconciler) CreateKokotapper(ctx context.Context, req ctrl.Request, vtap *networkingv1alpha1.Kokotap) (corev1.Pod, error) {
+
+	logger := log.FromContext(ctx)
 	var pod corev1.Pod
-	podName := vtap.Spec.PodName
+	tappedPodName := "kokotapped_" + vtap.Spec.PodName
 
 	// Get PodInfo
 
 	podInfo := getPodInfo(vtap)
 
+	// Create the pod
+
+	pod = corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tappedPodName,
+			Namespace: vtap.Namespace,
+			Labels: map[string]string{
+				"dn-vtap": "tapped",
+			},
+		},
+		Spec: corev1.PodSpec{
+			HostNetwork: true,
+			NodeName:    podInfo.nodeName,
+			Volumes: []corev1.Volume{
+				{
+					Name: "proc",
+					VolumeSource: corev1.VolumeSource{
+						HostPath: &corev1.HostPathVolumeSource{
+							Path: "/proc",
+						},
+					},
+				},
+				{
+					Name: "var-crio",
+					VolumeSource: corev1.VolumeSource{
+						HostPath: &corev1.HostPathVolumeSource{
+							Path: "/var/run/crio/crio.sock",
+						},
+					},
+				},
+			},
+			Containers: []corev1.Container{
+				{
+					Name:  "kokotap-network-tap",
+					Image: vtap.Spec.Image,
+					SecurityContext: &corev1.SecurityContext{
+						Privileged: &[]bool{true}[0],
+					},
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:      "proc",
+							MountPath: "/host/proc",
+						},
+						{
+							Name:      "var-crio",
+							MountPath: "/var/run/crio/crio.sock",
+						},
+					},
+					Command: []string{"/bin/kokotap"},
+					Args: []string{
+						"--procprefix=/hostproc",
+						"mode",
+						"sender",
+						//TODO: Add a function to get containerID from podName
+						"--containerid=" + podInfo.containerID,
+						"--mirrortype=" + vtap.Spec.MirrorType,
+						"--ifname= mirror",
+						"--mirrorif=" + vtap.Spec.PodInterface,
+						"--pod=" + vtap.Spec.PodName,
+						"--namespace=" + vtap.Namespace,
+						//TODO: Add a function to get node's IP given podName (see kokotap)
+						"--vxlan-egressip=" + podInfo.nodeIP,
+						"--vxlan-ip=" + vtap.Spec.TargetIP,
+						"--vxlan-id=" + vtap.Spec.VxLANID,
+						"--vxlan-port=4789",
+					},
+				},
+			},
+		},
+	}
+
+	// Create the pod
+
+	if err := r.Create(ctx, &pod); err != nil {
+		logger.Error(err, "Failed to create Pod", "Pod.Namespace", vtap.Namespace, "Pod.Name", tappedPodName)
+		return pod, err
+	}
+	logger.Info("Created Pod successfully", "Pod.Namespace", vtap.Namespace, "Pod.Name", tappedPodName)
+	return pod, nil
+}
+
+func (r *KokotapReconciler) ReconcileNormal(ctx context.Context, req ctrl.Request, vtap *networkingv1alpha1.Kokotap) (ctrl.Result, error) {
+
+	logger := log.FromContext(ctx)
+
+	var pod corev1.Pod
+
 	// Check if the pod exists
 
 	err := r.Get(ctx, types.NamespacedName{Name: vtap.Spec.PodName, Namespace: vtap.Spec.Namespace}, &pod)
-	logger := log.FromContext(ctx)
+
 	if err != nil && client.IgnoreNotFound(err) != nil {
 		// Pod does not exist, create it
-		pod = corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      podName,
-				Namespace: vtap.Namespace,
-				Labels: map[string]string{
-					"dn-vtap": "tapped",
-				},
-			},
-			Spec: corev1.PodSpec{
-				HostNetwork: true,
-				NodeName:    podInfo.nodeName,
-				Volumes: []corev1.Volume{
-					{
-						Name: "proc",
-						VolumeSource: corev1.VolumeSource{
-							HostPath: &corev1.HostPathVolumeSource{
-								Path: "/proc",
-							},
-						},
-					},
-					{
-						Name: "var-crio",
-						VolumeSource: corev1.VolumeSource{
-							HostPath: &corev1.HostPathVolumeSource{
-								Path: "/var/run/crio/crio.sock",
-							},
-						},
-					},
-				},
-				Containers: []corev1.Container{
-					{
-						Name:  "kokotap-network-tap",
-						Image: vtap.Spec.Image,
-						SecurityContext: &corev1.SecurityContext{
-							Privileged: &[]bool{true}[0],
-						},
-						VolumeMounts: []corev1.VolumeMount{
-							{
-								Name:      "proc",
-								MountPath: "/host/proc",
-							},
-							{
-								Name:      "var-crio",
-								MountPath: "/var/run/crio/crio.sock",
-							},
-						},
-						Command: []string{"/bin/kokotap"},
-						Args: []string{
-							"--procprefix=/hostproc",
-							"mode",
-							"sender",
-							//TODO: Add a function to get containerID from podName
-							"--containerid=" + podInfo.containerID,
-							"--mirrortype=" + vtap.Spec.MirrorType,
-							"--ifname= mirror",
-							"--mirrorif=" + vtap.Spec.PodInterface,
-							"--pod=" + vtap.Spec.PodName,
-							"--namespace=" + vtap.Namespace,
-							//TODO: Add a function to get node's IP given podName (see kokotap)
-							"--vxlan-egressip=" + podInfo.nodeIP,
-							"--vxlan-ip=" + vtap.Spec.TargetIP,
-							"--vxlan-id=" + vtap.Spec.VxLANID,
-							"--vxlan-port=4789",
-						},
-					},
-				},
-			},
+		pod, err = r.CreateKokotapper(ctx, req, vtap)
+		if err != nil {
+			return ctrl.Result{}, err
 		}
+		logger.Error(err, "Failed to create Pod", "Pod.Namespace", vtap.Namespace, "Pod.Name", vtap.Spec.PodName)
 	}
-	if err := r.Create(ctx, &pod); err != nil {
-		logger.Error(err, "Failed to create Pod", "Pod.Namespace", vtap.Namespace, "Pod.Name", podName)
-		return (ctrl.Result{}), err
-	}
-	logger.Info("Created Pod successfully", "Pod.Namespace", vtap.Namespace, "Pod.Name", podName)
 	return ctrl.Result{}, nil
 
 }
 
 func (r *KokotapReconciler) ReconcileDelete(ctx context.Context, req ctrl.Request, vtap *networkingv1alpha1.Kokotap) (ctrl.Result, error) {
-	// Logic shall be added here
+	// Delete the resources
+
 	return ctrl.Result{}, nil
 }
 
@@ -215,6 +256,7 @@ func GetClientSet(clientset *kubernetes.Clientset) *KokotapReconciler {
 }
 
 func getPodInfo(vtap *networkingv1alpha1.Kokotap) *PodInfo {
+
 	//Get cluster config
 
 	config, err := rest.InClusterConfig()
